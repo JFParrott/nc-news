cf9e1a492f307b7cdde13d641210cd1c9a2c42b1
JFParrott <jfparrott@protonmail.com>
2020-10-16 12:50:34 +0100

    add heroku hosting config

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -13,13 +13,14 @@ describe('/api', () => {
     return connection.seed.run();
   });
   after(() => {
-    connection.destroy();
+    return connection.destroy();
   });
-  it('GET: 200 and object containing object of available endpoints', () => {
+  it.only('GET: 200 and object containing object of available endpoints', () => {
     return request(app)
       .get('/api/')
       .expect(200)
-      .then(res => {
+      .then((res) => {
+        console.log(res.body);
         const { endpoints } = res.body;
         expect(endpoints).to.be.an('array');
         expect(endpoints[0]).to.contain.keys('path', 'methods');
@@ -29,17 +30,17 @@ describe('/api', () => {
     return request(app)
       .get('/api/rewards')
       .expect(404)
-      .then(err => {
+      .then((err) => {
         expect(err.body.msg).to.equal('Route not found');
       });
   });
   it('status: 405 and "Method not allowed" message', () => {
     const invalidMethods = ['patch', 'put', 'delete', 'post'];
-    const methodPromises = invalidMethods.map(method => {
+    const methodPromises = invalidMethods.map((method) => {
       return request(app)
         [method]('/api')
         .expect(405)
-        .then(res => {
+        .then((res) => {
           expect(res.body.msg).to.equal('Method not allowed');
         });
     });
@@ -50,7 +51,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/topics')
         .expect(200)
-        .then(res => {
+        .then((res) => {
           const { topics } = res.body;
           expect(topics).to.be.an('array');
           expect(topics[0]).to.contain.keys('slug', 'description');
@@ -59,11 +60,11 @@ describe('/api', () => {
     });
     it('status: 405 and "Method not allowed" message', () => {
       const invalidMethods = ['patch', 'put', 'delete', 'post'];
-      const methodPromises = invalidMethods.map(method => {
+      const methodPromises = invalidMethods.map((method) => {
         return request(app)
           [method]('/api/topics')
           .expect(405)
-          .then(res => {
+          .then((res) => {
             expect(res.body.msg).to.equal('Method not allowed');
           });
       });
@@ -76,7 +77,7 @@ describe('/api', () => {
         return request(app)
           .get('/api/users/lurker')
           .expect(200)
-          .then(res => {
+          .then((res) => {
             const { user } = res.body;
             expect(user).to.contain.keys('username', 'name', 'avatar_url');
             expect(user.username).to.equal('lurker');
@@ -86,17 +87,17 @@ describe('/api', () => {
         return request(app)
           .get('/api/users/bob')
           .expect(404)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Invalid Username');
           });
       });
       it('status: 405 and "Method not allowed" message', () => {
         const invalidMethods = ['patch', 'post', 'put', 'delete'];
-        const methodPromises = invalidMethods.map(method => {
+        const methodPromises = invalidMethods.map((method) => {
           return request(app)
             [method]('/api/users/1')
             .expect(405)
-            .then(res => {
+            .then((res) => {
               expect(res.body.msg).to.equal('Method not allowed');
             });
         });
@@ -109,7 +110,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles')
         .expect(200)
-        .then(res => {
+        .then((res) => {
           const { articles } = res.body;
           expect(articles).to.be.an('array');
           expect(articles[0]).to.contain.keys(
@@ -123,7 +124,7 @@ describe('/api', () => {
           );
           expect(articles).to.have.length(12);
           expect(articles).to.be.sortedBy('created_at', {
-            descending: true
+            descending: true,
           });
         });
     });
@@ -131,7 +132,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?sort_by=article_id&order=asc')
         .expect(200)
-        .then(res => {
+        .then((res) => {
           expect(res.body.articles).to.be.sortedBy('article_id');
         });
     });
@@ -139,7 +140,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?author=butter_bridge')
         .expect(200)
-        .then(res => {
+        .then((res) => {
           const { articles } = res.body;
           expect(articles[0].author).to.equal('butter_bridge');
           expect(articles).to.have.length(3);
@@ -149,7 +150,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?topic=mitch')
         .expect(200)
-        .then(res => {
+        .then((res) => {
           const { articles } = res.body;
           expect(articles[0].topic).to.equal('mitch');
           expect(articles).to.have.length(11);
@@ -159,7 +160,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?author=rogersop&topic=mitch')
         .expect(200)
-        .then(res => {
+        .then((res) => {
           const { articles } = res.body;
           expect(articles[0].topic).to.equal('mitch');
           expect(articles[0].author).to.equal('rogersop');
@@ -170,7 +171,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?topic=paper')
         .expect(200)
-        .then(res => {
+        .then((res) => {
           const { articles } = res.body;
           expect(articles).to.be.an('array');
           expect(articles).to.have.length(0);
@@ -180,7 +181,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?author=lurker')
         .expect(200)
-        .then(res => {
+        .then((res) => {
           const { articles } = res.body;
           expect(articles).to.be.an('array');
           expect(articles).to.have.length(0);
@@ -190,7 +191,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?sort_by=beethoven')
         .expect(400)
-        .then(err => {
+        .then((err) => {
           expect(err.body.msg).to.equal('Invalid input');
         });
     });
@@ -198,7 +199,7 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?author=beethoven')
         .expect(404)
-        .then(err => {
+        .then((err) => {
           expect(err.body.msg).to.equal('Invalid Username');
         });
     });
@@ -206,17 +207,17 @@ describe('/api', () => {
       return request(app)
         .get('/api/articles?topic=beethoven')
         .expect(404)
-        .then(err => {
+        .then((err) => {
           expect(err.body.msg).to.equal('Topic does not exist');
         });
     });
     it('status: 405 and "Method not allowed" message', () => {
       const invalidMethods = ['patch', 'put', 'post', 'delete'];
-      const methodPromises = invalidMethods.map(method => {
+      const methodPromises = invalidMethods.map((method) => {
         return request(app)
           [method]('/api/articles')
           .expect(405)
-          .then(res => {
+          .then((res) => {
             expect(res.body.msg).to.equal('Method not allowed');
           });
       });
@@ -227,7 +228,7 @@ describe('/api', () => {
         return request(app)
           .get('/api/articles/1')
           .expect(200)
-          .then(res => {
+          .then((res) => {
             const { article } = res.body;
             expect(article.article_id).to.equal(1);
             expect(article.comment_count).to.equal('13');
@@ -237,7 +238,7 @@ describe('/api', () => {
         return request(app)
           .get('/api/articles/99')
           .expect(404)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Article ID does not exist');
           });
       });
@@ -245,7 +246,7 @@ describe('/api', () => {
         return request(app)
           .get('/api/articles/fish')
           .expect(400)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Bad request');
           });
       });
@@ -254,7 +255,7 @@ describe('/api', () => {
           .patch('/api/articles/2')
           .send({ inc_votes: -10 })
           .expect(200)
-          .then(res => {
+          .then((res) => {
             const { article } = res.body;
             expect(article).to.contain.keys(
               'article_id',
@@ -274,7 +275,7 @@ describe('/api', () => {
           .patch('/api/articles/99')
           .send({ inc_votes: 5 })
           .expect(404)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Article ID does not exist');
           });
       });
@@ -283,7 +284,7 @@ describe('/api', () => {
           .patch('/api/articles/fish')
           .send({ inc_votes: 10 })
           .expect(400)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Bad request');
           });
       });
@@ -292,7 +293,7 @@ describe('/api', () => {
           .patch('/api/articles/3')
           .send({ votes: 5 })
           .expect(400)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Invalid input');
           });
       });
@@ -301,17 +302,17 @@ describe('/api', () => {
           .patch('/api/articles/1')
           .send({ inc_votes: 'blue' })
           .expect(400)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Invalid input');
           });
       });
       it('status: 405 and "Method not allowed" message', () => {
         const invalidMethods = ['put', 'post', 'delete'];
-        const methodPromises = invalidMethods.map(method => {
+        const methodPromises = invalidMethods.map((method) => {
           return request(app)
             [method]('/api/articles/1')
             .expect(405)
-            .then(res => {
+            .then((res) => {
               expect(res.body.msg).to.equal('Method not allowed');
             });
         });
@@ -322,7 +323,7 @@ describe('/api', () => {
           return request(app)
             .get('/api/articles/1/comments')
             .expect(200)
-            .then(res => {
+            .then((res) => {
               const { comments } = res.body;
               expect(comments).to.be.an('array');
               expect(comments[0]).to.contain.keys(
@@ -333,7 +334,7 @@ describe('/api', () => {
                 'body'
               );
               expect(comments).to.be.sortedBy('created_at', {
-                descending: true
+                descending: true,
               });
               expect(comments).to.be.length(13);
             });
@@ -342,7 +343,7 @@ describe('/api', () => {
           return request(app)
             .get('/api/articles/1/comments?sort_by=author&order=asc')
             .expect(200)
-            .then(res => {
+            .then((res) => {
               expect(res.body.comments).to.be.sortedBy('author');
             });
         });
@@ -350,7 +351,7 @@ describe('/api', () => {
           return request(app)
             .get('/api/articles/2/comments')
             .expect(200)
-            .then(res => {
+            .then((res) => {
               const { comments } = res.body;
               expect(comments).to.be.an('array');
               expect(comments).to.have.length(0);
@@ -360,7 +361,7 @@ describe('/api', () => {
           return request(app)
             .get('/api/articles/99/comments')
             .expect(404)
-            .then(err => {
+            .then((err) => {
               expect(err.body.msg).to.equal('Article ID does not exist');
             });
         });
@@ -368,7 +369,7 @@ describe('/api', () => {
           return request(app)
             .get('/api/articles/blue/comments')
             .expect(400)
-            .then(err => {
+            .then((err) => {
               expect(err.body.msg).to.equal('Bad request');
             });
         });
@@ -376,9 +377,9 @@ describe('/api', () => {
           return request(app)
             .get('/api/articles/1/comments?sort=author')
             .expect(200)
-            .then(res => {
+            .then((res) => {
               expect(res.body.comments).to.be.sortedBy('created_at', {
-                descending: true
+                descending: true,
               });
             });
         });
@@ -387,7 +388,7 @@ describe('/api', () => {
             .post('/api/articles/1/comments')
             .send({ username: 'rogersop', body: 'rather good' })
             .expect(201)
-            .then(res => {
+            .then((res) => {
               const { comment } = res.body;
               expect(comment).to.contain.keys(
                 'article_id',
@@ -408,7 +409,7 @@ describe('/api', () => {
             .post('/api/articles/99/comments')
             .send({ username: 'rogersop', body: 'rather good' })
             .expect(404)
-            .then(err => {
+            .then((err) => {
               expect(err.body.msg).to.equal('Not found');
             });
         });
@@ -417,7 +418,7 @@ describe('/api', () => {
             .post('/api/articles/blue/comments')
             .send({ username: 'rogersop', body: 'rather good' })
             .expect(400)
-            .then(err => {
+            .then((err) => {
               expect(err.body.msg).to.equal('Bad request');
             });
         });
@@ -426,7 +427,7 @@ describe('/api', () => {
             .post('/api/articles/1/comments')
             .send({ username: null, body: 'good' })
             .expect(400)
-            .then(err => {
+            .then((err) => {
               expect(err.body.msg).to.equal('Invalid input');
             });
         });
@@ -435,17 +436,17 @@ describe('/api', () => {
             .post('/api/articles/1/comments')
             .send({ name: 'rogersop', body: 'rather good' })
             .expect(400)
-            .then(err => {
+            .then((err) => {
               expect(err.body.msg).to.equal('Invalid input');
             });
         });
         it('status: 405 "Method not allowed" message', () => {
           const invalidMethods = ['patch', 'put', 'delete'];
-          const methodPromises = invalidMethods.map(method => {
+          const methodPromises = invalidMethods.map((method) => {
             return request(app)
               [method]('/api/articles/1/comments')
               .expect(405)
-              .then(res => {
+              .then((res) => {
                 expect(res.body.msg).to.equal('Method not allowed');
               });
           });
@@ -461,7 +462,7 @@ describe('/api', () => {
           .patch('/api/comments/1')
           .send({ inc_votes: 10 })
           .expect(200)
-          .then(res => {
+          .then((res) => {
             const { comment } = res.body;
             expect(comment.comment_id).to.equal(1);
             expect(comment).to.contain.keys(
@@ -480,7 +481,7 @@ describe('/api', () => {
           .patch('/api/comments/99')
           .send({ inc_votes: 10 })
           .expect(404)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Comment ID does not exist');
           });
       });
@@ -489,7 +490,7 @@ describe('/api', () => {
           .patch('/api/comments/blue')
           .send({ inc_votes: 5 })
           .expect(400)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Bad request');
           });
       });
@@ -498,7 +499,7 @@ describe('/api', () => {
           .patch('/api/comments/1')
           .send({ votes: 5 })
           .expect(400)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Invalid input');
           });
       });
@@ -507,20 +508,18 @@ describe('/api', () => {
           .patch('/api/comments/1')
           .send({ inc_votes: 'blue' })
           .expect(400)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Invalid input');
           });
       });
       it('DELETE: 204', () => {
-        return request(app)
-          .delete('/api/comments/1')
-          .expect(204);
+        return request(app).delete('/api/comments/1').expect(204);
       });
       it('DELETE: 404 and "Comment ID does not exist" message when non-existent comment_id provided', () => {
         return request(app)
           .delete('/api/comments/999')
           .expect(404)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Comment ID does not exist');
           });
       });
@@ -528,17 +527,17 @@ describe('/api', () => {
         return request(app)
           .delete('/api/comments/blue')
           .expect(400)
-          .then(err => {
+          .then((err) => {
             expect(err.body.msg).to.equal('Bad request');
           });
       });
       it('status: 405 and "Method not allowed" message', () => {
         const invalidMethods = ['get', 'post', 'put'];
-        const methodPromises = invalidMethods.map(method => {
+        const methodPromises = invalidMethods.map((method) => {
           return request(app)
             [method]('/api/comments/2')
             .expect(405)
-            .then(res => {
+            .then((res) => {
               expect(res.body.msg).to.equal('Method not allowed');
             });
         });
